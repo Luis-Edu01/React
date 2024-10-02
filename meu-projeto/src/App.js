587eb7a95c371c625d5a550b6820d0b55b0eeb8c
Luís Eduardo <146043378+Luis-Edu01@users.noreply.github.com>
2024-10-02 19:12:01 -0300

    React + CSS

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import Dados from './components/Dados'
+import './App.css'
 
 export default function App() {
 
   return (
-      <>
-        <Dados/>
-      </>
+    <>
+      <section calssName='caixa'>
+        <h1>Eu sou mt bom</h1>
+        <h2>Curso de React</h2>
+        <p className='texto'>Eu sou bom demais</p>
+        <a href='https://www.youtube.com' target='_blanck'>Teste Youtube</a>
+      </section>
+    </>
   )
 }
