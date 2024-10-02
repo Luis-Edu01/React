ad30054d35a379edc93e2965ff1bccf21fecf716
Luís Eduardo <146043378+Luis-Edu01@users.noreply.github.com>
2024-10-02 20:16:40 -0300

    renderização do relogio com atualização a cada segundo

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import Relogio from './components/Relogio'
 import './App.css'
 
 export default function App() {
 
   return (
     <>
-      <section calssName='caixa'>
+      <section className='caixa'>
         <h1>Eu sou mt bom</h1>
         <h2>Curso de React</h2>
         <p className='texto'>Eu sou bom demais</p>
         <a href='https://www.youtube.com' target='_blanck'>Teste Youtube</a>
+        <Relogio/>
       </section>
     </>
   )
