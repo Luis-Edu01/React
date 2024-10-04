6e8a7e3077cd6fdf42f1ecc6136740169bb0640e
Luís Eduardo <146043378+Luis-Edu01@users.noreply.github.com>
2024-10-03 21:38:12 -0300

    .

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,20 +1,10 @@
-import React,{useState} from 'react'
-import Imgs from './components/Imgs'
+import React from 'react'
 import './App.css'
 
 export default function App() {
-
-  const [ligado,setLigado] = useState(false)
-
-  const cancelar=(obj)=>{
-    return obj.preventDefault()
-  }
-
   return (
     <>
-      <Imgs ligado={ligado} setLigado={setLigado}/>
 
-      <a href='https://youtube.com' target='_blank' onClick={(e)=>cancelar(e)}>Teste Link cancelado</a>
     </>
   )
 }
